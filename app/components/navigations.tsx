ece52bce863a6c9aa1082a1a1240852aec287620
dagmawibabi <1babidagi@gmail.com>
2023-07-31 14:58:01 +0300

    removed quote

--- app/components/navigations.tsx
+++ app/components/navigations.tsx
@@ -5,7 +5,7 @@ export default function Navigation () {
     return (
         <div className="flex justify-between mb-10 sticky top-0 bg-[#0A0A0A] z-50 py-5">
             <span className="text-3xl">
-                Hi, I'm Dagmawi Babi 👋
+                {"Hi, I'm Dagmawi Babi 👋"}
             </span>
             <div className="w-44 flex justify-between">
                 <FontAwesomeIcon icon={faTelegramPlane} className="text-zinc-500 hover:text-white hover:scale-125" width={23} height={23} />
