061a9537cbdf61ccfcb28e25f05346e6dfc2a2a3
dagmawibabi <1babidagi@gmail.com>
2023-08-08 16:04:15 +0300

    didn't work

--- app/blogs/[title]/page.tsx
+++ app/blogs/[title]/page.tsx
@@ -1,24 +1,37 @@
 'use client';
-import {remark} from 'remark'
-import remarkGfm from 'remark-gfm'
-import remarkToc from 'remark-toc'
-import html from 'remark-html';
 
-export default async function (blog:any) {
-    // {decodeURI(blog.params.title)}
-    let file = await fetch("https://raw.githubusercontent.com/dagmawibabi/dblogs/main/public/blogs/Sin.md")
-    console.log(file)
+import Footer from "@/app/components/footer";
+import Navigation from "@/app/components/navigations";
 
-    const processedContent = await remark()
-    .use(remarkGfm)
-    .use(remarkToc)
-    .use(html)
-    .process('# Hi\n\n## Table of contents\n\n## Hello\n\n*Some* ~more~ _things_.');
-    const contentHtml = processedContent.toString();
+
+export default function (param: any) {
+
+    let file = "./Sin.html";
 
     return (
-        <div>
-            <div dangerouslySetInnerHTML={{ __html: contentHtml }} />
+        <div className="flex bg-[#0A0A0A] ">
+            <div className="bg-[#0A0A0A] w-fit blur-3xl absolute top-0 left-0 ">
+                <div className="sticky top-0 m-10 w-80 h-80 bg-red-500 opacity-70 rounded-full"></div>
+            </div>
+
+            <div className="w-6/12 mx-auto h-fit overflow-scroll pt-10 pb-4 px-10 bg-[#0A0A0A] z-40">
+                <Navigation />
+                <div className="text-zinc-400">
+                    July 12, 2024
+                </div>
+                <div className="text-white text-2xl font-bold">
+                    {param.params.title.toString().replace(/%20/g, " ")}
+                </div>
+                <div className="h-screen text-white pb-96">
+                    <iframe src="./Sin.html" className="h-screen text-white bg-[#0A0A0A]" width={"100%"} ></iframe>
+                </div>
+                <Footer />
+            </div>
+
+            <div className="bg-[#0A0A0A] w-fit blur-3xl absolute bottom-0 right-0">
+                <div className=" m-10 w-80 h-80 bg-blue-500 opacity-70 rounded-full"></div>
+            </div>
+
         </div>
     )
 }
--- app/components/navigations.tsx
+++ app/components/navigations.tsx
@@ -1,12 +1,15 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faGithub, faInstagram, faLinkedin, faLinkedinIn, faTelegram, faTelegramPlane, faTiktok } from "@fortawesome/free-brands-svg-icons";
+import Link from "next/link";
 
-export default function Navigation () {
+export default function Navigation() {
     return (
         <div className="flex justify-between mb-10 sticky top-0 bg-[#0A0A0A] z-50 py-5">
-            <span className="text-3xl">
-                {"Hi, I'm Dagmawi Babi 👋"}
-            </span>
+            <Link href={"/"}>
+                <span className="text-3xl hover:underline hover:underline-offset-8">
+                    {"Hi, I'm Dagmawi Babi 👋"}
+                </span>
+            </Link>
             <div className="w-44 flex justify-between">
                 <FontAwesomeIcon icon={faTelegramPlane} className="text-zinc-500 hover:text-white hover:scale-125" width={23} height={23} />
                 <FontAwesomeIcon icon={faInstagram} className="text-zinc-500 hover:text-white hover:scale-125" width={23} height={23} />
