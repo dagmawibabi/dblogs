e02289f2731f104757d93432f7b57a436f17f979
dagmawibabi <1babidagi@gmail.com>
2023-08-10 08:25:25 +0300

    md supported

--- app/blogs/[title]/page.tsx
+++ app/blogs/[title]/page.tsx
@@ -74,7 +74,7 @@ export default async function (param: any) {
                     { renderableData.value }
                 </div> */}
 
-                <ReactMarkdown remarkPlugins={[remarkMdx, remarkGfm, remarkBehead, remarkBreaks, remarkToc]}>
+                <ReactMarkdown >
                     {renderableDataString}
                 </ReactMarkdown>
 
