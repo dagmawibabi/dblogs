f626a1f21b1dd1cc073524b1b191d69a53495060
dagmawibabi <1babidagi@gmail.com>
2023-08-11 11:49:21 +0300

    tab responsive

--- app/blogs/[title]/page.tsx
+++ app/blogs/[title]/page.tsx
@@ -100,11 +100,21 @@ export default async function (param: any) {
 
     return (
         <div className="flex bg-[#0A0A0A] " suppressHydrationWarning >
-            <div className="bg-[#0A0A0A] w-fit blur-3xl absolute top-0 left-0 ">
+            <div className="
+        bg-[#0A0A0A] blur-3xl absolute top-0 left-0 w-fit hidden 
+          sm:w-fit md:w-fit lg:w-fit xl:w-fit
+          sm:hidden md:hidden lg:block xl:block
+        ">
                 <div className="sticky top-0 m-10 w-80 h-80 bg-red-500 opacity-70 rounded-full"></div>
             </div>
 
-            <div className="w-6/12 mx-auto h-fit overflow-hidden pt-10 pb-4 px-10 bg-[#0A0A0A] z-40">
+            {/* <div className="w-6/12 mx-auto h-fit overflow-hidden pt-10 pb-4 px-10 bg-[#0A0A0A] z-40"> */}
+            <div className="w-full h-fit overflow-hidden pt-2 pb-4 px-5 bg-[#0A0A0A] z-40 mx-0 
+                sm:pt-10
+                sm:px-10
+                sm:w-full lg:w-6/12 xl:w-6/12 
+                sm:mx-auto md:mx-auto lg:mx-auto xl:mx-auto
+            ">
                 <Navigation />
                 {/* DATE */}
                 <div className="text-zinc-400 text-sm mb-5">
@@ -112,7 +122,10 @@ export default async function (param: any) {
                 </div>
 
                 {/* BLOG TITLE */}
-                <div className="text-white text-2xl font-bold mb-5">
+                <div className="
+                    text-white text-xl font-bold mb-5
+                    sm:text-2xl
+                ">
                     {param.params.title.toString().replace(/%20/g, " ")}
                 </div>
 
@@ -144,7 +157,10 @@ export default async function (param: any) {
                 <Footer />
             </div>
 
-            <div className="bg-[#0A0A0A] w-fit blur-3xl absolute bottom-0 right-0">
+            <div className="
+                bg-[#0A0A0A] blur-3xl absolute bottom-0 right-0 w-fit hidden 
+                sm:hidden md:hidden lg:block xl:block
+            ">
                 <div className=" m-10 w-80 h-80 bg-blue-500 opacity-70 rounded-full"></div>
             </div>
 
--- app/page.tsx
+++ app/page.tsx
@@ -13,7 +13,7 @@ export default function Home() {
       <div className="
         bg-[#0A0A0A] blur-3xl absolute top-0 left-0 w-fit hidden 
           sm:w-fit md:w-fit lg:w-fit xl:w-fit
-          sm:block md:block lg:block xl:block
+          sm:hidden md:hidden lg:block xl:block
         ">
         <div className="sticky top-0 m-10 w-80 h-80 bg-red-500 opacity-70 rounded-full"></div>
       </div>
@@ -21,7 +21,7 @@ export default function Home() {
       <div className="w-full h-fit overflow-hidden pt-2 pb-4 px-5 bg-[#0A0A0A] z-40 mx-0 
         sm:pt-10
         sm:px-10
-        sm:w-6/12 
+        sm:w-full lg:w-6/12 xl:w-6/12 
         sm:mx-auto md:mx-auto lg:mx-auto xl:mx-auto
       ">
         <Navigation />
@@ -32,7 +32,7 @@ export default function Home() {
 
       <div className="
         bg-[#0A0A0A] blur-3xl absolute bottom-0 right-0 w-fit hidden 
-        sm:block md:block lg:block xl:block
+        sm:hidden md:hidden lg:block xl:block
       ">
         <div className=" m-10 w-80 h-80 bg-blue-500 opacity-70 rounded-full"></div>
       </div>
