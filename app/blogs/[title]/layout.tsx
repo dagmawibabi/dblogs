7e89dc2b9a8e8649d8f4c7cbc4a32d9d79e54db9
dagmawibabi <1babidagi@gmail.com>
2023-08-19 17:39:33 +0300

    og xp didn't work

--- app/blogs/[title]/layout.tsx
+++ app/blogs/[title]/layout.tsx
@@ -1,31 +1,4 @@
 import "../../globals.css";
-import type { Metadata } from "next";
-import configJSON from "../../../public/config.json";
-
-export const metadata: Metadata = {
-  metadataBase: new URL(configJSON.website.toString()),
-  title: "testing link previews",
-  description: "this would be the description db xd",
-  openGraph: {
-    title: "testing link previews",
-    description: "this would be the description db xd",
-    siteName: "testing link previews",
-    images: [
-      {
-        url: `/og?title=${"testing link previews"}`,
-        width: 800,
-        height: 600,
-      },
-      {
-        url: `/og?title=${"testing link previews"}`,
-        width: 1600,
-        height: 1200,
-      },
-    ],
-    locale: "en_US",
-    type: "website",
-  },
-};
 
 export default function BlogLayout({
   children,
